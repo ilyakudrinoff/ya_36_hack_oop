6b99be5e0c413479100c9343f060aeab40fde042
ilyakudrinofficial <ilyakudrinofficial@mail.ru>
2022-03-20 20:25:55 +0700

    Выполение 1 Шага и 2 Шага

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+from typing import Type, Dict
 from dataclasses import dataclass
 
 
@@ -40,5 +41,34 @@ class Warrior(Person):
         self.base_attack = base_attack * 2
 
 
+def create_thing():
+    name = {'щит': [0.05, 0.08, 0.02], 'меч': [0.04, 0.08, 0.03],
+            'броня': [0.08, 0.01, 0.01], 'кольчуга': [0.06, 0.01, 0.02],
+            'дубинка': [0.03, 0.06, 0.02], 'сапоги': [0.01, 0.01, 0.01]
+            }
+    random_index = random.randint(0, len([name.keys()]) - 1)
+    return Thing(name[random_index], *name[random_index])
+
+
+def create_person() -> Person:
+    name_persons = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
+                    'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z']
+    persons = {'WR': Warrior, 'PL': Paladin}
+    index_person = random.randint(0, len([persons.keys()]) - 1)
+    for i in range(1, 10):
+        random_index = random.randint(0, len(name_persons) - 1)
+        return persons[index_person](name_persons[random_index],
+                                     random.randint(0, 10), 2, 2)
+
+
+def main():
+    my_things = []
+    for thing in range(1, random.randint(2, 10)):
+        my_things.append(create_thing())
+
+    print(f'{person_attack} наносит удар по '
+          f'{person_protect} на {count_hp} урона.')
+
+
 if __name__ == '__main__':
-    pass
+    main()
